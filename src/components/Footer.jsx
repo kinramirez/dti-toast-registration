b431a18f85b534c26974fbf69722c9b05e36338a
ljnacional <ljnacional01@gmail.com>
2026-07-06 16:25:06 +0800

    chore: update contact strip phone, email, and social details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,15 +16,9 @@ const InstagramIcon = () => (
   </svg>
 );
 
-const TwitterIcon = () => (
+const TikTokIcon = () => (
   <svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-    <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
-  </svg>
-);
-
-const LinkedInIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+    <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"/>
   </svg>
 );
 
@@ -49,17 +43,14 @@ export const Footer = () => {
               FOLLOW US
             </span>
             <div className="flex items-center gap-4" aria-label="Social links">
-              <a href="#" aria-label="Facebook" className="inline-flex items-center justify-center transition-opacity hover:opacity-80">
+              <a href="https://www.facebook.com/toastweddingfair" aria-label="Facebook" className="inline-flex items-center justify-center transition-opacity hover:opacity-80">
                 <FacebookIcon />
               </a>
-              <a href="#" aria-label="Instagram" className="inline-flex items-center justify-center transition-opacity hover:opacity-80">
+              <a href="https://www.instagram.com/toastweddingfair?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" aria-label="Instagram" className="inline-flex items-center justify-center transition-opacity hover:opacity-80">
                 <InstagramIcon />
               </a>
-              <a href="#" aria-label="Twitter" className="inline-flex items-center justify-center transition-opacity hover:opacity-80">
-                <TwitterIcon />
-              </a>
-              <a href="#" aria-label="LinkedIn" className="inline-flex items-center justify-center transition-opacity hover:opacity-80">
-                <LinkedInIcon />
+              <a href="https://www.tiktok.com/@toastweddingfair" aria-label="TikTok" className="inline-flex items-center justify-center transition-opacity hover:opacity-80">
+                <TikTokIcon />
               </a>
             </div>
           </div>
